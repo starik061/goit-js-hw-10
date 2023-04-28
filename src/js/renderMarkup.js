8980b6e24c8bc8b2a68473d23495862d6e7bb71d
starik061 <starik061@gmail.com>
2023-04-28 16:03:40 +0300

    fix country language displaying

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -16,7 +16,15 @@ export function renderOneCountry(coutriesArray) {
   let coutriesListHTML = coutriesArray
     .map(
       ({ flags, name, capital, population, languages }) =>
-        `<p><img src="${flags.svg}" width="20px" height="20px"><span> ${name.official}</span><span> ${capital}</span><span> ${population}</span><span>${languages}</span></p>`
+        `<p><img src="${flags.svg}" width="40px" height="40px">
+        <br>
+        <span> ${name.official}</span>
+        <br>
+        <span>Capital: ${capital}</span>
+        <br>
+        <span>Population: ${population}</span>
+        <br>
+        <span>Language: ${Object.values(languages).join(' ')}</span></p>`
     )
     .join('');
 
